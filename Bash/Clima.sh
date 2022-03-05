7b62751c2dfbadc4b62bf4db51ab54acac4607ec
Josue Gomez <gerardo.gomez@live.com>
2022-03-04 23:50:10 -0600

    Modificacion de los archivos Bash

--- Bash/Clima.sh
+++ Bash/Clima.sh
@@ -3,12 +3,12 @@
 echo ""
 echo "             ***|| ESTADO DEL TIEMPO CIUDAD DE GUATEMALA ||***       "
 echo ""
-touch "InfoClima"
+touch "InformacionClima"
 #Tomamos la información de la página y ya creado el 
 #archivo InfoClima, se guarda la información allí
-echo "$(curl wttr.in/Guatemala)" > "InfoClima"
+echo "$(curl wttr.in/Guatemala)" > "InformacionClima"
 #Escribimos las primeras  7 líneas y eliminamos el 
 #archivo
-head -7 InfoClima
-rm InfoClima
+head -7 InformacionClima
+rm InformacionClima
 echo ""
