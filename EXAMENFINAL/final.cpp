af51052e52f3237cd25d36596b9061a7b345c585
Josue Gomez <gerardo.gomez@live.com>
2022-05-16 14:06:11 -0600

    chore: cambios

--- EXAMENFINAL/final.cpp
+++ EXAMENFINAL/final.cpp
@@ -33,10 +33,10 @@ float y, vel, mf, Mc, dy, F, E, g, rho;
 float densidad(float rho);
 float gravedad(float g);
 float fircca(float Fa);
-float consumogas(float dm);
-float vm(float mc);
+float consumogas(float TSFC, float Eo);
+float mc(float mo, float consumogas());
 float ef(float e);
-float edo(float dy);
+float EDO(float dy);
 
 // 6. Presentacion de datos
 
@@ -75,33 +75,30 @@ void Cohete::despegar(){
     FILE* datosvel1;
     FILE* datosvel2;
     FILE* datosvel3;
-    //apertura de los archivos, Y corresponde a la altura, y 1 al numero de cohete: Y_1, altura para cohete  1
-    datosy1 = fopen("Datos_Y_1","wt");
-    datosvel1 = fopen("Datos_Vel_1","wt");
-    datosy2 = fopen("Datos_Y_2","wt");
-    datosvel2 = fopen("Datos_Vel_2","wt");
-    datosy3 = fopen("Datos_Y_3","wt");
-    datosvel3 = fopen("Datos_Vel_3","wt");
+    //apertura de los archivos, Y corresponde a la altura, y 1 al numero de cohete: Y, altura para cohete  1
+    datosy1 = fopen("DatosY1","wt");
+    datosvel1 = fopen("DatosVel1","wt");
+    datosy2 = fopen("DatosY2","wt");
+    datosvel2 = fopen("DatosVel2","wt");
+    datosy3 = fopen("DatosY3","wt");
+    datosvel3 = fopen("DatosVel3","wt");
     
     //Calculo de y: altura y v: velocidad
     while (i < 1800 && y > Ho)
     {
         //escritura de datos mientras se ejecuta el programa
-        fprintf(datosy1, "%lf\t %lf \n", delta*i, y);
-        fprintf(datosy2, "%lf\t %lf \n", delta*i, y);
-        fprintf(datosy3, "%lf\t %lf \n", delta*i, y);
-        fprintf(datosvel1, "%lf\t %lf \n", delta*i, y);
-        fprintf(datosvel2, "%lf\t %lf \n", delta*i, y);
-        fprintf(datosvel3, "%lf\t %lf \n", delta*i, y);
+        fprintf(datosy1, "%f\t %f \n", delta*i, y);
+        fprintf(datosy2, "%f\t %f \n", delta*i, y);
+        fprintf(datosy3, "%f\t %f \n", delta*i, y);
+        fprintf(datosvel1, "%f\t %f \n", delta*i, y);
+        fprintf(datosvel2, "%f\t %f \n", delta*i, y);
+        fprintf(datosvel3, "%f\t %f \n", delta*i, y);
 
         //calculo de velocidad y altura
         vel = vel + delta*EDO(dy)*i;
-        y = y+delta*vel*i
+        y = y+delta*vel*i;
         i++;
     }
-    
-
-
 }
 
 //4. Funciones
@@ -133,19 +130,18 @@ float fricca(float Ffr){
 }
 
 //4.4 consumo del combustible
-float consumogas(float dm){
+float consumogas(float TSFC, float Eo){
     float resp; 
     resp =-TSFC*Eo;
     return resp;
 }
 
 //4.5 para la variacion de la masa
-float vm(float mc){
-    float mc;
-    mc = mo + consumogas(y);
-    return mc;
+float mc(float mo, float consumogas()){
+    float resp;
+    resp = mo+consumogas();
+    return resp;
 }
-
 //4.6
 float ef(float e){
     if (mf == 0)
@@ -170,7 +166,7 @@ float EDO(float dy){
 void grafica(){
     FILE * graf;
     graf=fopen("grafica.gp", "wt");
-    fprintf(graf, "plot 'grafica.txt'");
+    fprintf(graf, "plot 'grafica'");
     fclose(graf);
 }
 int main(){
@@ -180,8 +176,8 @@ int main(){
     c1.despegar();
     c2.despegar();
     c3.despegar();
-    system("gnuplot -p grafica.gp");
-    system("replot datosy1.dat, datosy2.dat, datosy3.dat, datosvel1.dat, datosvel2.dat, datosvel3.dat");
+    system("gnuplot grafica.gp");
+    system("plot DatosY1, DatosY2, DatosY3, datosVel1, datosVel2, datosVel3");
 }
 
 
